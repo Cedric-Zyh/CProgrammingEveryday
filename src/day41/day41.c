141f5f8260457e8d4b04439ebf19d00c34e7391c
Z-P-J <37832345+Z-P-J@users.noreply.github.com>
2019-12-03 21:56:21 +0800

    update day41.c

--- src/day41/day41.c
+++ src/day41/day41.c
@@ -6,9 +6,10 @@
 
 void fun() {
     int i = 0;
-    static static_i = 0;
+    static int static_i = 0;
     printf("i = %d\n", i);
-    printf("static_i = %d\n", static_i);
+    printf("fun static_i = %d\n", static_i);
+    printf("fun &static_i = %p\n", &static_i);
     i++;
     static_i++;
 }
@@ -16,6 +17,11 @@ void fun() {
 int main() {
     for (int i = 0; i < 3; i++) {
         fun();
+        // 该static_i与fun函数中的static_i不是同一个静态变量
+        static int static_i = 0;
+        static_i++;
+        printf("main static_i = %d\n", static_i);
+        printf("main &static_i = %p\n", &static_i);
     }
     system("pause");
     return 0;
